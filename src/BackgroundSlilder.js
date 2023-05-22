de94623c2a8c62133aabc8479c22c6f42e649464
carlos <carlosjuniorrvega@hotmail.com>
2023-05-22 15:55:23 -0500

    fix:corrected dynamic background image.CV

--- src/BackgroundSlilder.js
+++ src/BackgroundSlilder.js
@@ -2,7 +2,7 @@ import { useState,useEffect} from 'react';
 import './BackgroundSlider.css';
 import imageSlide from './data';
 
-const BackgroundSlilder = () => {
+const BackgroundSlider = () => {
   const [currentState, setCurrentState] = useState(0);
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -15,7 +15,7 @@ const BackgroundSlilder = () => {
     return () => clearTimeout(timer);
   }, [currentState]);
   const bgImageStyle = {
-    backgroundImage: `url(${imageSlide[0].url})`,
+    backgroundImage: `url(${imageSlide[currentState].url})`,
     backgroundPosition: 'center',
     backgroundSize: 'cover',
     height: '100%',
@@ -44,4 +44,56 @@ const BackgroundSlilder = () => {
   );
 };
 
-export default BackgroundSlilder;
+export default BackgroundSlider;
+// import React, { useState, useEffect } from 'react';
+// import './BackgroundSlider.css';
+// import imageSlide from './data';
+
+// const BackgroundSlider = () => {
+//   const [currentState, setCurrentState] = useState(0);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       if (currentState === 2) {
+//         setCurrentState(0);
+//       } else {
+//         setCurrentState(currentState + 1);
+//       }
+//     }, 5000);
+
+//     return () => clearTimeout(timer);
+//   }, [currentState]);
+
+//   const bgImageStyle = {
+//     backgroundImage: `url(${imageSlide[currentState].url})`,
+//     backgroundPosition: 'center',
+//     backgroundSize: 'cover',
+//     height: '100%',
+//   };
+
+//   const goToNext = (state) => {
+//     setCurrentState(state);
+//   };
+
+//   return (
+//     <div className="container-style">
+//       <div style={bgImageStyle}></div>
+//       <div className="transparent-background"></div>
+//       <div className="description">
+//         <div>
+//           <h1>{imageSlide[currentState].title}</h1>
+//           <p>{imageSlide[currentState].body}</p>
+//         </div>
+//         <div className="carousel-bullet">
+//           {imageSlide.map((slide, index) => (
+//             <span key={index} onClick={() => goToNext(index)}>
+//               {index + 1}
+//             </span>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default BackgroundSlider;
